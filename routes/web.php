91bc3bda335090e130adf10b2f8c2b80e7cc72fe
utqkaba <utqkaba@gmail.com>
2026-04-15 23:11:43 +0300

    remove template auth files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\LeagueSimulatorController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LeagueSimulatorController::class, 'teams'])->name('simulator.teams');
@@ -11,12 +10,3 @@ Route::get('/simulation', [LeagueSimulatorController::class, 'simulation'])->nam
 Route::post('/simulation/play-next-week', [LeagueSimulatorController::class, 'playNextWeek'])->name('simulator.play-next-week');
 Route::post('/simulation/play-all-weeks', [LeagueSimulatorController::class, 'playAllWeeks'])->name('simulator.play-all-weeks');
 Route::post('/simulation/reset-data', [LeagueSimulatorController::class, 'resetData'])->name('simulator.reset-data');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [LeagueSimulatorController::class, 'teams'])->name('dashboard');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
